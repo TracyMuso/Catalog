147ca27f68f64875b9b9db00a62cf2b7e9d12054
Megha Bodke <megha.n.bodke@gmail.com>
2022-11-29 14:21:14 +0530

    Create item class

---
+++ Classes/item.rb
@@ -0,0 +1,28 @@
+class Item
+    attr_reader :genre, :author, :source, :label, :publish_date, :
+    def initialize
+       @id = Random.rand(1..1000)
+        @genre = genre
+        @author = author
+        @source = source
+        @label = label
+        @publish = publish
+        @publish_date = publish_date
+        @archived = false
+    end
+
+def move_to_archive
+@archieved = true if can_be_archieved?
+end
+
+    private
+    def can_be_archived?
+        year_pub = Time.parse(@publish_date).year
+        current_date = Time.new.year
+        current_date - year_pub > 10 ? true : false
+    end
+
+
+
+
+end
