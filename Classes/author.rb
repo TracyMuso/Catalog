bc5f9035d5a4343b84d0016761d376c93af452f1
Bateyjosue <josuebatey19@gmail.com>
2022-11-29 16:57:12 +0200

    Fix Linters

--- Classes/author.rb
+++ Classes/author.rb
@@ -1,6 +1,7 @@
 class Author
   attr_reader :items, :id
   attr_accessor :first_name, :last_name
+
   def initialize(first_name, last_name)
     @id = Random.rand(1..1000)
     @first_name = first_name
--- Classes/game.rb
+++ Classes/game.rb
@@ -2,16 +2,18 @@ require './item'
 
 class Game < Item
   attr_accessor :multiplayer, :last_player_at
+
   def initialize(multiplayer, last_player_at)
     super()
     @multiplayer = multiplayer
     @last_player_at = last_player_at
   end
+
   private
+
   def can_be_archieved?
     last = Time.parse(@last_player_at).year
     now = Time.new.year
     now - last > 2 && super.can_be_archieved?
   end
 end
-
