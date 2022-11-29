2336cf585a0f0fb4630540dbd317a18f6b209830
Megha Bodke <megha.n.bodke@gmail.com>
2022-11-29 15:08:29 +0530

    Update item.rb

--- Classes/item.rb
+++ Classes/item.rb
@@ -1,15 +1,21 @@
 class Item
-  attr_reader :genre, :author, :source, :label, :publish_date
+  attr_accessor :publish_date 
+  attr_reader :genre, :author, :source, :label
 
   def initialize(publish_date)
     @id = Random.rand(1..1000)
     @genre = nil
+    @genre.items << self
     @author = nil
+    @author.items << self
     @source = nil
+    @source.items << self
     @label = nil
+    @label.items << self
     @publish_date = publish_date
     @archived = false
   end
+
 
   def move_to_archive
     @archieved = true if can_be_archieved?
@@ -22,4 +28,6 @@ class Item
     current_date = Time.new.year
     current_date - year_pub > 10
   end
+
+
 end
