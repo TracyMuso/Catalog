60f881ef7c0c700b5a7b883bca46f1b736c2a771
Megha Bodke <megha.n.bodke@gmail.com>
2022-11-29 14:35:16 +0530

    Fixed linters

--- Classes/item.rb
+++ Classes/item.rb
@@ -1,28 +1,25 @@
 class Item
-    attr_reader :genre, :author, :source, :label, :publish_date, :
-    def initialize
-       @id = Random.rand(1..1000)
-        @genre = genre
-        @author = author
-        @source = source
-        @label = label
-        @publish = publish
-        @publish_date = publish_date
-        @archived = false
-    end
+  attr_reader :genre, :author, :source, :label, :publish_date
 
-def move_to_archive
-@archieved = true if can_be_archieved?
-end
+  def initialize (publish_date)
+    @id = Random.rand(1..1000)
+    @genre = genre
+    @author = author
+    @source = source
+    @label = nil
+    @publish_date = publish_date
+    @archived = false
+  end
 
-    private
-    def can_be_archived?
-        year_pub = Time.parse(@publish_date).year
-        current_date = Time.new.year
-        current_date - year_pub > 10 ? true : false
-    end
+  def move_to_archive
+    @archieved = true if can_be_archieved?
+  end
 
+  private
 
-
-
+  def can_be_archived?
+    year_pub = Time.parse(@publish_date).year
+    current_date = Time.new.year
+    current_date - year_pub > 10
+  end
 end
