d773a0c92b086a0bdd1827d500ea86008771cab7
kidde60 <kgeorgewilliam60@gmail.com>
2022-11-30 11:01:58 +0300

    Remove linter error

--- Classes/book.rb
+++ Classes/book.rb
@@ -1,18 +1,19 @@
- require_relative 'item'
+require_relative 'item'
 class Book < Item
-    attr_accessor :publisher, :cover_state
+  attr_accessor :publisher, :cover_state
 
-    def initialize(publish_date, publisher, cover_state)
-        super(publish_date)
-        @publisher = publisher
-        @cover_state = cover_state
-    end
-    private
-    def can_be_archived?
-     super || cover_state == 'bad'
-    end
+  def initialize(publish_date, publisher, cover_state)
+    super(publish_date)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  private
+
+  def can_be_archived?
+    super || cover_state == 'bad'
+  end
 end
 
 book1 = Book.new('2022-04-03', 'Joan', 'good')
 puts book1.label
-
