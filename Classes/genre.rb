6a970fe608d032efa8b2fe1e1943a89558cc389b
Elfin Git <musongoletracy@gmail.com>
2022-11-30 09:01:03 +0200

    add methods to app.rb

--- Classes/genre.rb
+++ Classes/genre.rb
@@ -1,17 +1,18 @@
 require_relative 'item'
 
 class Genre < Item
-    attr_accessor :name
-    attr_reader :items, :id
-    
-    def initialize(name)
-        @name = name
-        @items = []
-        @id = Random.rand(1..1000)
-    end
+  attr_accessor :name
+  attr_reader :items, :id
 
-    def add_item(item)
-        @items << item
-        item.add_genre(self)
-    end
+  def initialize(name)
+    super()
+    @name = name
+    @items = []
+    @id = Random.rand(1..1000)
+  end
+
+  def add_item(item)
+    @items << item
+    item.add_genre(self)
+  end
 end
--- Classes/music_album.rb
+++ Classes/music_album.rb
@@ -1,18 +1,18 @@
 require_relative 'item'
 
 class MusicAlbum < Item
-    attr_accessor :name, :on_spotify
+  attr_accessor :name, :on_spotify
 
-    def initialize(name, on_spotify, publish_date)
-        super(publish_date)
-        @name = name
-        @on_spotify = on_spotify
-        @publish_date = publish_date
-    end
+  def initialize(name, on_spotify, publish_date)
+    super(publish_date)
+    @name = name
+    @on_spotify = on_spotify
+    @publish_date = publish_date
+  end
 
-    def can_be_archived?
-       super && @on_spotify
-    end
+  def can_be_archived?
+    super && @on_spotify
+  end
 end
 
 # ruby Classes/music_album.rb
