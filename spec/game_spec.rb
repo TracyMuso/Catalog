64ee7463d881ffb0f25b01349e7215ddb116d8d8
Bateyjosue <josuebatey19@gmail.com>
2022-11-30 16:25:02 +0200

    Add Test for Game Class

---
+++ spec/game_spec.rb
@@ -0,0 +1,20 @@
+require '../classes/game'
+
+# Unit test class Game all methods
+describe Game do
+  content "Unit Test Game Class" do
+    game = Game.mew '2022-11-25', false, '2022-11-30'
+    it 'Should return Game publish Date' do
+      expect(game.publish_date).to eq('2022-11-25')
+    end
+
+    it 'Should return game multiplayer' do
+      expect(game.multiplayer).to be false
+    end
+
+    it 'Should be an instance og Game' do
+      expect(game).to be_an_instance_of(Game)
+    end
+
+  end
+end
